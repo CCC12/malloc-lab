bd8aa3ce486dfbadcff806d238bf72f7b2ef8f21
Alvin <alvin.ye.y@gmail.com>
2017-09-06 20:30:02 -0400

    Stuided the macros for manipulating the free list

--- mm.c
+++ mm.c
@@ -23,33 +23,63 @@
  * provide your team information in the following struct.
  ********************************************************/
 team_t team = {
-    /* Team name */
-    "yolo",
-    /* First member's full name */
-    "ccc12",
-    /* First member's email address */
-    "myemail",
-    /* Second member's full name (leave blank if none) */
-    "",
-    /* Second member's email address (leave blank if none) */
-    ""
+	/* Team name */
+	"yolo",
+	/* First member's full name */
+	"ccc12",
+	/* First member's email address */
+	"myemail",
+	/* Second member's full name (leave blank if none) */
+	"",
+	/* Second member's email address (leave blank if none) */
+	""
 };
 
+#define WSIZE		4
+#define DSIZE		8
+
 /* single word (4) or double word (8) alignment */
-#define ALIGNMENT 8
+#define ALIGNMENT	8
 
 /* rounds up to the nearest multiple of ALIGNMENT */
-#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
+#define ALIGN(size)			(((size) + (ALIGNMENT-1)) & ~0x7)
 
+/* Pack size(multiple of 8) with an allocate bit */
+#define PACK(size, alloc)	((size) | (alloc))
+
+/* Put a word into where p points at */
+#define PUT(p, val)			(*(unsigned int *)(p) = (val))
+#define GET(p)				(*(unsigned int *)(p))
+
+/* Get size or alloca bit of the block */
+#define GET_SIZE(p)			(GET(p) & 0b1000)
+#define GET_ALLOC(p)		(GET(p) & 0b01)
+
+/* 
+ * Operates on block pointer which points to the first byte of the payload
+ * comput its header and footer
+ */
+#define HDRP(bp)			((char *)(bp) - WSIZE)
+#define FTRP(bp)			((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+
+#define NEXT_BLKP(bp)		((char *)(bp) + GET_SIZE((char *)(bp) - WSIZE))
+#define PREV_BLKP(bp)		((char *)(bp) - GET_SIZE((char *)(bp) - DSIZE))
+
+/* Put a w
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+
+static void *heap_listp = NULL; /* pointer to Prologue block */
 
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
-    return 0;
+	if ((heap_listp = mem_sbrk(4*WSIZE)) == (void *) -1) 
+		return -1;
+
+	return 0;
 }
 
 /* 
@@ -58,14 +88,14 @@ int mm_init(void)
  */
 void *mm_malloc(size_t size)
 {
-    int newsize = ALIGN(size + SIZE_T_SIZE);
-    void *p = mem_sbrk(newsize);
-    if (p == (void *)-1)
-	return NULL;
-    else {
-        *(size_t *)p = size;
-        return (void *)((char *)p + SIZE_T_SIZE);
-    }
+	int newsize = ALIGN(size + SIZE_T_SIZE);
+	void *p = mem_sbrk(newsize);
+	if (p == (void *)-1)
+		return NULL;
+	else {
+		*(size_t *)p = size;
+		return (void *)((char *)p + SIZE_T_SIZE);
+	}
 }
 
 /*
@@ -80,19 +110,19 @@ void mm_free(void *ptr)
  */
 void *mm_realloc(void *ptr, size_t size)
 {
-    void *oldptr = ptr;
-    void *newptr;
-    size_t copySize;
-    
-    newptr = mm_malloc(size);
-    if (newptr == NULL)
-      return NULL;
-    copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
-    if (size < copySize)
-      copySize = size;
-    memcpy(newptr, oldptr, copySize);
-    mm_free(oldptr);
-    return newptr;
+	void *oldptr = ptr;
+	void *newptr;
+	size_t copySize;
+
+	newptr = mm_malloc(size);
+	if (newptr == NULL)
+		return NULL;
+	copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
+	if (size < copySize)
+		copySize = size;
+	memcpy(newptr, oldptr, copySize);
+	mm_free(oldptr);
+	return newptr;
 }
 
 
