e7a864a24f70255750c7f07ba1118f56cf1ba293
Alvin <alvin.ye.y@gmail.com>
2017-09-06 22:56:08 -0400

    fix mm.c

--- mm.c
+++ mm.c
@@ -83,9 +83,10 @@ int mm_init(void)
 	/* Setting up the empty list with padding word 
 	 * and Prologue and Epilogue blocks
 	 */
+
 	if ((heap_listp = mem_sbrk(4*WSIZE)) == (void *) -1)	
 		return -1;
-	PUT(heap_listp, 0);								/* paddding word */
+	PUT(heap_listp, 0);									/* paddding word */
 	PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1));		/* Prologue block */
 	PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));
 	PUT(heap_listp + (3*WSIZE), PACK(0, 1));			/* Epilogue block */
